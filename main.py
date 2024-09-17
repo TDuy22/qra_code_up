b69810dba5ccb44b40696b232561b82d2c822c63
TDuy22 <phamthanhduyvbhp@gmail.com>
2024-09-17 22:42:26 +0700

    a

--- main.py
+++ main.py
@@ -123,6 +123,7 @@ def evaluate(iter_cnt, filepath, model, corpus, args, logging=True):
     auc_meter = AUCMeter()
     scores = [ np.asarray([], dtype='float32') for i in range(2) ]
     for loader_id, loader in enumerate((neg_batch_loader, pos_batch_loader)):
+        print(loader)
         for data in loader:
             data = map(corpus.get, data)
             batch = None
